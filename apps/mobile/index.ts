3cd73df4adbed4efd699cfd7bece51af559102c9
InvolveX <lukaswill97@gmail.com>
2026-01-23 00:52:33 +0100

    fix(mobile): add crypto polyfill and fix shared imports

--- apps/mobile/index.ts
+++ apps/mobile/index.ts
@@ -1,5 +1,6 @@
 import "partysocket/event-target-polyfill";
 import "react-native-url-polyfill/auto";
+import "react-native-get-random-values";
 import "web-streams-polyfill/polyfill";
 import "expo-router/entry";
 import "text-encoding";
