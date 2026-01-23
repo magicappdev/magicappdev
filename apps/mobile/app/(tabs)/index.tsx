6c7158ce87afc69e6c308c86edf13957839f434d
InvolveX <lukaswill97@gmail.com>
2026-01-23 01:33:20 +0100

    fix(mobile): resolve android build path length issues

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Link } from "expo-router";
 
